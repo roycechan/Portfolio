57d592392b8f5b90a828225782381f2313234889
roycechan <roycechan89@gmail.com>
2019-03-17 13:59:07 -0400

    Update score.py

--- Face Classification and Verification with Thumbnail Images/score.py
+++ Face Classification and Verification with Thumbnail Images/score.py
@@ -8,15 +8,15 @@ def get_auc(y_true, y_score):
 
 
 if __name__ == '__main__':
-    # args = sys.argv[1:]
-    # if len(args) < 2:
-    #     print("python score.py <score_csv> <label_csv>")
-    #     exit()
+    args = sys.argv[1:]
+    if len(args) < 2:
+        print("python score.py <score_csv> <label_csv>")
+        exit()
     args = pd.read_csv("output_verification.csv",index_col=False)
     print(args.iloc[:,0])
-    # score_csv = args[1]
-    # label_csv = args[2]
-    # y_score = pd.read_csv(score_csv).score.values
-    # y_true = pd.read_csv(label_csv).score.values
-    # auc = get_auc(y_true, y_score)
-    # print("AUC: ", auc)
+    score_csv = args[1]
+    label_csv = args[2]
+    y_score = pd.read_csv(score_csv).score.values
+    y_true = pd.read_csv(label_csv).score.values
+    auc = get_auc(y_true, y_score)
+    print("AUC: ", auc)
